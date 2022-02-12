deb631727b4934c263efccc4ceb5069205b4c720
Ahsan Habib <58974742+armanahsanhabib@users.noreply.github.com>
2022-02-12 15:31:44 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+    // The data/time we want to countdown to
+    var countDownDate = new Date("Feb 13, 2022 11:29:30").getTime();
+
+    // Run myfunc every second
+    var myfunc = setInterval(function() {
+
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+        
+    // Calculating the days, hours, minutes and seconds left
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+        
+    // Result is output to the specific element
+    document.getElementById("days").innerHTML = days + "d "
+    document.getElementById("hours").innerHTML = hours + "h " 
+    document.getElementById("mins").innerHTML = minutes + "m " 
+    document.getElementById("secs").innerHTML = seconds + "s " 
+        
+    // Display the message when countdown is over
+    if (timeleft < 0) {
+        clearInterval(myfunc);
+        document.getElementById("days").innerHTML = ""
+        document.getElementById("hours").innerHTML = ""
+        document.getElementById("mins").innerHTML = ""
+        document.getElementById("secs").innerHTML = ""
+        document.getElementById("end").innerHTML = "Result is now published.";
+        document.getElementById("timer").style.display="none"; 
+    }
+}, 1000);  
+
+function getResultAll() {
+    document.getElementById("result_page").style.display="block"; 
+} 
